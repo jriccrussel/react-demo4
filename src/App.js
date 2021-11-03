106842b007d95cb026fb9d32c6407decbf8e9bf2
jriccrussel <jriccrussel@gmail.com>
2021-11-04 07:17:55 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.scss'
+import Content from './components/Content'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+     <Header />
+     <Content />
+    </>
+  )
+}
+
+export default App
