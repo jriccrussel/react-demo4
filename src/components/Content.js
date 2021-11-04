4b1578b1a948f8e024ab603598ae4bdf42f0448f
jriccrussel <jriccrussel@gmail.com>
2021-11-04 21:57:12 +0800

    percent scroll

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,99 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import gsap from 'gsap'
+import { TweenLite } from 'gsap/gsap-core'
 
 const Content = () => {
+
+    useEffect(() => {
+        let counter = document.querySelector(".percent")
+
+        // let tl = new TweenLite()
+        // let tl = gsap.timeline()
+
+        TweenLite.set(counter, {
+            xPercent: -5,
+            yPercent: -5,
+        });
+
+        window.addEventListener("mousemove", moveCounter);
+
+        function moveCounter(e) {
+            TweenLite.to(counter, 0.5, {
+                x: e.clientX,
+                y: e.clientY,
+            });
+        }
+
+        // Mouse Move Setup
+        // let moveCounter = (e) => {
+        //     TweenLite.set(counter, {
+        //         xPercent: -5,
+        //         yPercent: -5,
+        //     })
+        // }
+
+        let progress = () => {
+            let windowScrollTop = document.documentElement.scrollTop
+            let docHeight = document.documentElement.offsetHeight   
+            let windowHeight = window.innerHeight
+            let progress = (windowScrollTop / (docHeight - windowHeight)) * 100
+
+            let bgColor = progress > 99 ? "#fff" : "#fff"
+            let txtColor = progress > 99 ? "#fff" : "#333"
+
+            // document.querySelector("h1").text(Math.round(progress) + "%").css({ color: $txtColor })
+
+            // document.querySelector(".fill").text(Math.round(progress) + "%").css({ color: $bgColor })      
+            
+            let text = document.querySelector("h1")
+            let fill = document.querySelector(".fill")
+
+            document.querySelector("h1").innerHTML = Math.round(progress) + "%" 
+            document.querySelector("h1").style.color = bgColor
+            
+            document.querySelector(".fill").innerHTML = Math.round(progress) + "%"     
+            document.querySelector(".fill").style.color = txtColor
+            
+        }
+
+        progress()
+
+        document.addEventListener("scroll", progress)
+
+    }, [])
+
     return (
-        <div>
-            Content
-        </div>
+        <>
+            <div className="percent">
+                <div className="fill"></div>
+                <div className="counter"><h1></h1></div>
+            </div>
+            <div className="container">
+                <div className="content">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus enim
+                        dolorem quod asperiores quam. Itaque obcaecati blanditiis ut. Nobis vel illo
+                        amet fugiat rerum distinctio placeat ratione et! Adipisci voluptates est nulla,
+                        nostrum nesciunt nisi placeat recusandae! Consequatur, rem.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, minima accusamus
+                        odit voluptas cumque et maxime. Non nam minima tempora nemo assumenda fugit
+                        illum repellendus facilis. Placeat possimus tempora sapiente, pariatur nisi rem
+                        voluptatibus quaerat nulla, dolorem sunt maiores, itaque illum nam neque!
+                        Inventore optio repellat fugit vel odit ea.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit inventore quam
+                        est, illo rem voluptatem neque incidunt quos eos? Quis ex minima quam autem et
+                        odit at dolores officia modi.
+                    </p>
+                </div>
+            </div>
+        </>
     )
 }
 
 export default Content
+
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,83 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import imgOne from '../assets/img/menu-img-one.jpg'
+import imgTwo from '../assets/img/menu-img-two.jpg'
+import imgThree from '../assets/img/menu-img-three.jpg'
+import imgFour from '../assets/img/menu-img-four.jpg'
+import gsap from 'gsap'
+import { TimelineMax } from 'gsap/gsap-core'
+
+
+const data = [
+    {img: imgOne, name: "Home", path: "/",},
+    {img: imgTwo, name: "Showcase", path: "/",},
+    {img: imgThree, name: "About", path: "/",},
+    {img: imgFour, name: "Contact", path: "/",},
+]
 
 const Header = () => {
+
+    useEffect(() => {
+
+        // let tl = new TimelineMax({paused: true})
+        let tl = gsap.timeline({paused: true})
+        
+        tl.to(".nav-container", 1, {
+            left: 0,
+            ease: "expo.easeInOut",
+        })
+
+        tl.staggerFrom(".menu > div", 0.8, {
+            y: 100,
+            opacity: 0,
+            ease: "expo.easeInOut",
+        }, "0.1", "-=0.4")
+
+        tl.staggerFrom(".socials", 0.8, {
+            y: 100,
+            opacity: 0,
+            ease: "expo.easeInOut",
+        }, "0.4", "-=0.6")
+
+        tl.reverse()
+
+        document.querySelector(".menu-open").addEventListener("click", () => tl.reversed(!tl.reversed()))
+
+        document.querySelector(".menu-close").addEventListener("click", () => tl.reversed(!tl.reversed()))
+
+    }, [])
+
     return (
-        <div>
-            Header
-        </div>
+        <>
+            <div className="menu-open">menu</div>
+
+            <div className="nav-container">
+                <div className="menu-close">close</div>
+                <div className="socials">
+                    <span>facebook</span>
+                    <span>instagram</span>
+                </div>
+
+                <div className="menu">
+                    {data.map((item, index) => {
+                        const {img, name, path} = item
+                        return (
+                            <div className="menu__item" key={index}>
+                                <Link className="menu__item-link" to={path}>{name}</Link>
+                                <img className="menu__item-img" src={img} alt="" />
+                                <div className="marquee">
+                                    <div className="marquee__inner">
+                                        <span>{name} - {name} - {name} - {name} - {name} - Home - Home</span>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+
+            </div>
+        </>
     )
 }
 
